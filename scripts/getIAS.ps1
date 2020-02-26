b4fdbc32c411681b639216bdf0e6bbcca66ea791
Jarrett Goldfedder <jgoldfed@msn.com>
2020-02-25 21:24:06 -0500

    First version

---
+++ scripts/getIAS.ps1
@@ -0,0 +1,21 @@
+﻿# Create a web service proxy and attach the user certificate to it
+$pwd = "niku123PMAT"
+$securepwd = $pwd | ConvertTo-SecureString -asPlainText -Force
+
+$certificate = Import-PfxCertificate -FilePath "C:\Users\JarrettGoldfedder\Documents\Jarrett\PowerShell\Provisioning\scripts\SATLAPPMATAP01.med.ds.osd.mil.pfx" -CertStoreLocation Cert:\CurrentUser\My -Password $securepwd
+
+$Body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:uas="http://uas.ias.com/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <uas:getIASID>
+         <id>1510135616</id>
+         <idType>E</idType>
+         <returnIdType>U</returnIdType>
+      </uas:getIASID>
+   </soapenv:Body>
+</soapenv:Envelope>'
+$request = Invoke-WebRequest -Uri "https://cacpt.csd.disa.mil:443/ECRSWebServices/uas?wsdl" -Headers (@{SOAPAction='Read'}) -Method Post -Body $Body -ContentType text/xml  -Certificate $certificate 
+
+$pattern = ".*<id>(.*)</id>.*"
+$id = [regex]::match($request.Content, $pattern).Groups[1].Value
+$id + '@example.com'
---
+++ scripts/parsePDF.ps1
@@ -0,0 +1,46 @@
+﻿[System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\..\apps\itextsharp.dll") | Out-Null
+Add-Type -Path "$PSScriptRoot\..\apps\itextsharp.dll"
+
+
+function ReadProperties {
+  param($content)
+  $AppProps = ConvertFrom-StringData (Get-Content $content -Raw)
+  return $AppProps
+}
+
+function getPDFField {
+  param($fieldname)
+  $outvalue = "Field Not Found"
+  for ($page = 1; $page -le $reader.NumberOfPages; $page++)
+  {
+    if ($page -eq 1) {
+      #extract a page and split it into lines
+      foreach ($PDFField in $PdfReader.AcroFields.Fields.Keys) {
+        #Uncheck the below to get back all the fields
+        #Write-Host $PDFField , ':', $PdfReader.AcroFields.getField($PDFField)
+
+        if ($PDFField -eq $fieldname) {
+          $outvalue = $PdfReader.AcroFields.GetField($PDFField)
+        }
+      }
+    }
+  }
+  return $outvalue
+}
+
+$props = ReadProperties ("$PSScriptRoot\..\properties\PortalUsers.properties")
+
+$PDFPath2785 = $props["PDFPath2785"]
+
+$PdfReader = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList "$PDFPath2785/Approved/mmiller_Ektropy SAAR_rev20191021-sign3.pdf"
+
+$EDIPI = getPDFField ($props["PDFFieldEDIPI"])
+$FullName= getPDFField ($props["PDFFieldName"])
+$Lname=$FullName.split(" ,")[0]
+$Fname=$FullName.split(" ")[1]
+$Mname=$FullName.split(" ")[2]
+
+Write-Host $fName,$Mname, $LName, '-', $EDIPI
+$reader.Close()
+
+
